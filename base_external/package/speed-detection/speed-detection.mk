3941666a025097b75cbeb1b75e97feaa22d35572
Krishna Suhagiya <krishna.suhagiya@colorado.edu>
2024-04-28 16:29:58 -0600

    Correct speed-detection.mk
    Signed-off-by: Krishna Suhagiya <krishna.suhagiya@colorado.edu>

--- base_external/package/speed-detection/speed-detection.mk
+++ base_external/package/speed-detection/speed-detection.mk
@@ -5,13 +5,13 @@
 ##############################################################
 
 
-DISPLAY_VERSION = 65fdc2c36efc50c12dbbbd53bc5c89f6b754a715
-DISPLAY_SITE = git@github.com:krishnasuhagiya29/final-project-assignment-krishna-suhas.git
-DISPLAY_SITE_METHOD = git
-DISPLAY_GIT_SUBMODULES = YES
+SPEED_DETECTION_VERSION = 65fdc2c36efc50c12dbbbd53bc5c89f6b754a715
+SPEED_DETECTION_SITE = git@github.com:krishnasuhagiya29/final-project-assignment-krishna-suhas.git
+SPEED_DETECTION_SITE_METHOD = git
+SPEED_DETECTION_GIT_SUBMODULES = YES
 
 # Adding the python script to detect the speed from the sign board
-define DISPLAY_INSTALL_TARGET_CMDS
+define SPEED_DETECTION_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/speed-detection/speed-limit-detection.py $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/speed-detection/60.png $(TARGET_DIR)/usr/bin
 endef
